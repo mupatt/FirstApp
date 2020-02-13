b5b7edcfa75bc5fd7a7caea002ebdcd61009c55a
PATO <mupatt@gmail.com>
2020-02-13 10:26:14 +0300

    MY app

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void sendMessage(View view){
+        EditText message = (EditText)findViewById(R.id.message);
+        Toast.makeText(this,"Sending message" + message.getText().toString(),Toast.LENGTH_SHORT).show();
+        Intent intent= new Intent(this,DisplayMessageActivity.class);
+        intent.putExtra("MESSAGE",message.getText().toString());
+        startActivity(intent);
+        message.setText("");
+    }
 }
+
